d4d996d4f5eca78502fa86008a52b87e04dd92a7
Sonst Michael <sourcing1@corporate-startup.com>
2023-11-25 14:31:41 +0100

    applies new build structure to build

--- download-server/src/main/java/com/cs/download/server/PluginController.java
+++ download-server/src/main/java/com/cs/download/server/PluginController.java
@@ -1,52 +1,37 @@
 package com.cs.download.server;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/plugins")
 public class PluginController {
 
-  private final DownloaderConfiguration mConfig;
+//  private final DownloaderConfiguration mConfig;
 
-  public PluginController(DownloaderConfiguration config) {
-    mConfig = config;
+//  public PluginController(DownloaderConfiguration config) {
+//    mConfig = config;
 
-    PluginServiceRegistry mServiceRegistry = new PluginServiceRegistry();
-        mServiceRegistry.addPluginPath("./plugin");
-        mServiceRegistry.start();
-  }
+//    PluginServiceRegistry mServiceRegistry = new PluginServiceRegistry();
+//        mServiceRegistry.addPluginPath("./plugin");
+//        mServiceRegistry.start();
+//  }
 
-  @PostMapping("/upload")
-  @ResponseBody
-  public String upload(@RequestParam("file") MultipartFile file) throws IOException {
-    File targetFile = new File(mConfig.pluginDir() + File.separator + file.getOriginalFilename());
-    OutputStream fileOutputStream = new FileOutputStream(targetFile);
-
-    FileCopyUtils.copy(file.getInputStream(), fileOutputStream);
-
-    return "ok";
-  }
-
-  @GetMapping("/list")
-  @ResponseBody
-  public List<String> listPlugins() {
-    return Arrays.stream(Objects.requireNonNull(new File("plugins").listFiles())).map(File::getName).collect(Collectors.toList());
-  }
+//  @PostMapping("/upload")
+//  @ResponseBody
+//  public String upload(@RequestParam("file") MultipartFile file) throws IOException {
+//    File targetFile = new File(mConfig.pluginDir() + File.separator + file.getOriginalFilename());
+//    OutputStream fileOutputStream = new FileOutputStream(targetFile);
+//
+//    FileCopyUtils.copy(file.getInputStream(), fileOutputStream);
+//
+//    return "ok";
+//  }
+//
+//  @GetMapping("/list")
+//  @ResponseBody
+//  public List<String> listPlugins() {
+//    return Arrays.stream(Objects.requireNonNull(new File("plugins").listFiles())).map(File::getName).collect(Collectors.toList());
+//  }
 
 }
